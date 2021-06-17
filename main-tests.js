a99fab80be7d58a662c4e36569b6e4d92e05b783
Rosano Coutinho <pub@xeari.com>
2021-06-17 09:37:25 -0300

    Spec OLSKBannerIsValid

--- main-tests.js
+++ main-tests.js
@@ -2,3 +2,42 @@ const { throws, deepEqual } = require('assert');
 
 const mod = require('./main.js');
 
+describe('OLSKBannerIsValid', function test_OLSKBannerIsValid () {
+
+	const _OLSKBannerIsValid = function (inputData = {}) {
+		return mod.OLSKBannerIsValid(Object.assign({
+			OLSKBannerBlurbHTML: Math.random().toString(),
+			OLSKBannerButtonText: Math.random().toString(),
+			OLSKBannerButtonLink: Math.random().toString(),
+		}, inputData));
+	};
+
+	it('throws if not object', function() {
+		throws(function() {
+			mod.OLSKBannerIsValid(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns false if OLSKBannerBlurbHTML not string', function () {
+		deepEqual(_OLSKBannerIsValid({
+			OLSKBannerBlurbHTML: null,
+		}), false);
+	});
+
+	it('returns false if OLSKBannerButtonText not string', function () {
+		deepEqual(_OLSKBannerIsValid({
+			OLSKBannerButtonText: null,
+		}), false);
+	});
+
+	it('returns false if OLSKBannerButtonLink not string', function () {
+		deepEqual(_OLSKBannerIsValid({
+			OLSKBannerButtonLink: null,
+		}), false);
+	});
+
+	it('returns true', function () {
+		deepEqual(_OLSKBannerIsValid(), true);
+	});
+	
+});
--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const mod = {
 
+	OLSKBannerIsValid (inputData) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof inputData.OLSKBannerBlurbHTML !== 'string') {
+			return false;
+		}
+
+		if (typeof inputData.OLSKBannerButtonText !== 'string') {
+			return false;
+		}
+
+		if (typeof inputData.OLSKBannerButtonLink !== 'string') {
+			return false;
+		}
+
+		return true;
+	},
 
 };
 
